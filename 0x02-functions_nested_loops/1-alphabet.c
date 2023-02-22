e8f53999cddfc57adc128135a40ae684cbdf2c29
Keketso Selete <keketsoselete@gmail.com>
2023-02-22 02:47:54 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet-Alphabets
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char c = 'a';
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	{
+		_putchar(c);
+
+	}
+
+	_putchar('\n');
+
+}
