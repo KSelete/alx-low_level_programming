e0218761a9defab610b2bf4b976b1bcbb168de8f
Keketso Selete <keketsoselete@gmail.com>
2023-02-22 04:31:57 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - check lowercase
+ *
+ * @c: character being checked
+ *
+ * Return: 1 if c is lowercase 0 if anything else
+ */
+
+int _slower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
