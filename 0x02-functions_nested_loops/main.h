aca32effaae20dc1395fb016a83d24f161f34738
Keketso Selete <keketsoselete@gmail.com>
2023-02-22 01:57:42 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-int add(int,int);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
