86587cb61559eab9edeb6dea35ad311d30cbbd59
Keketso Selete <keketsoselete@gmail.com>
2023-02-22 03:50:02 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,25 +5,18 @@
  * Return: void
  */
 
-
 void print_alphabet_x10(void)
 {
 	char c = 'a';
 	int n = 0;
 
 	for (n = 0; n < 10; n++)
-
 	{
-	
-	for (c = 'a'; c <= 'z'; c++)
-
-	{
-		_putchar(c);
-
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
-
-	}
-
 }
+
