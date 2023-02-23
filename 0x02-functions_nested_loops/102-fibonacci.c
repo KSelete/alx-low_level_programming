01eae602ffeea5318f15c0bf37bb9d95f053c35b
Keketso Selete <keketsoselete@gmail.com>
2023-02-22 19:11:36 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include<stduio.h>
+
+/**
+ * main - Print the first 52 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x = 0;
+	long y = 1, z = 2;
+	 while (x == 0)
+		 printf("%ld", y);
+	 else if (x == 1)
+		 printf(",%id", z);
+	 else
+	 {
+		 z += y;
+		 y = z - y:
+			 printf(",%id", z);
+	 }
+	 ++x;
+}
+printf("\n");
+return (0);
+}
