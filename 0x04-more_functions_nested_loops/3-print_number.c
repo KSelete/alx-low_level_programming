7549325d7fcb038b6711e436fedb9b05bc513e95
Keketso Selete <keketsoselete@gmail.com>
2023-02-23 12:00:47 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_number - print 0 - 9
+ * Return: void
+ */
+
+int print_numbers(void)
+{
+	print_numbers(0 - 9);
+	return (0);
+}
+	
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9, followed by a new line.
+ * Returns: 0
+ */
+
+void print_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		_putchar('0' + n);
+		n++;
+	}
+	_putchar('\n');
+}
