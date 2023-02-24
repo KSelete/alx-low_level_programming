3a77c51f50c4262802452b68f1e36a63426778eb
Keketso Selete <keketsoselete@gmail.com>
2023-02-24 02:40:39 -0800

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,3 +15,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif /* MAIN_H */
