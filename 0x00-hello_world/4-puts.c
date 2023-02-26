b2c6d79440eed58614c6e0baf9743078580ba920
Keketso Selete <keketsoselete@gmail.com>
2023-02-26 11:36:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  * main - A program that print
+ *   * Return: 0 (approved)
+ *    */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
