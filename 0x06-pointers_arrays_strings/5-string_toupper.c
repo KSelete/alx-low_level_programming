664a06547d9d06d77aaa7358beb777f15ec876be
Keketso Selete <keketsoselete@gmail.com>
2023-03-02 13:37:38 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters to uppercase
+ * code by KSelete
+ * @n: pointer
+ * Return: n
+ */
+
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
