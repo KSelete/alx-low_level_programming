83211e512535419cdcda28d020818b68decd5070
Keketso Selete <keketsoselete@gmail.com>
2023-03-02 15:29:58 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,9 +13,10 @@ int main(void)
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
-	 *- only one statement
-	 - you are not allowed to code anything else than this line of code/
-	 *(p + 5) = 98;
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code/
+	 */
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
