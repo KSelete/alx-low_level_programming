575a4e581c2c900579989fe651562607df353913
Keketso Selete <keketsoselete@gmail.com>
2023-03-02 12:30:30 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,6 +11,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+
 	int j;
 
 	j = 0;
@@ -24,6 +25,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = '\0';
 		j++;
 	}
-
-	return (dest)
+	return (dest);
 }
+
