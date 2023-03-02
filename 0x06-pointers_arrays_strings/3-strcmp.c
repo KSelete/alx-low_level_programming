2d8f1da273a7fefde7fad06ae8de5285d2eb0167
Keketso Selete <keketsoselete@gmail.com>
2023-03-02 12:37:02 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * code by KSelete
+ * @s1: the input value
+ * @s2: the input value
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	int = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
