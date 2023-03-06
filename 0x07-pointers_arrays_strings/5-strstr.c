10d9833bfb3952a834f1582d7aa080f4d5d0d68c
Keketso Selete <keketsoselete@gmail.com>
2023-03-06 14:30:02 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to be tested
+ * @needle: the substring to be searched for
+ * Return: returns a pointer to the beginning of the
+ * located substring, or NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j = 0, k;
+
+	for (i = 0; haystack[i] != 0; i++)
+	{
+		k = i;
+		j = 0;
+		for (; needle[j] != 0; )
+		{
+			if (haystack[k++] == needle[j++])
+			{
+				continue;
+			}
+			break;
+		}
+		if (needle[j] == '\0')
+			return ((haystack + i));
+	}
+	return ('\0');
+}
