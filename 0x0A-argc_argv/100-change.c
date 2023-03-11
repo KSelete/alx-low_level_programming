1031811f13dd585943e66bf3fa2e204e2479aa00
Keketso Selete <keketsoselete@gmail.com>
2023-03-11 07:46:37 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,46 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - a program that prints the minimum number of coins to make change
- * @argc: the number of command-line arguments
- * @argv: an array of strings that contain the command-line arguments
- * Return: returns 0 (sucessful), otherwise returns 1 on error
+ * main -main entry.
+ * @argc:number of command line arguments
+ * @argv: array containing the program command line arguments
+ * Description:program print minimum number of coins
+ * make change for amount mone
+ * Return: 1 if is not working correctly otherwise return 0
  */
-int main(int argc, char *argv[1])
-	int cents;
-	int quarters_25, dimes_10, nickles_5, coin_2, pennies_1;
-	int change;
-
-	if (argc !=2)
+int main(int argc, char **argv)
 {
-	printf("Error\n")
+	int n, change = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
 		return (1);
-}
-cents = atoi(argv[1]);
-
-if (cents < 0)
-{
-	printf("0\n");
+	}
+	else
+	{
+		n = atoi(argv[1]);
+		if (n < 0)
+		{
+			printf("0\n");
+		}
+		else
+		{
+			for (change = 0; n != 0; change++)
+			{
+				if (n - 25 >= 0)
+					n -= 25;
+				else if (n - 10 >= 0)
+					n -= 10;
+				else if (n - 5 >= 0)
+					n -= 5;
+				else if (n - 2 >= 0)
+					n -= 2;
+				else if (n - 1 >= 0)
+					n -= 1;
+			}
+			printf("%d\n", change);
+		}
+	}
 	return (0);
-}
- quarters_25 = cents / 25;
- cents = cents - (quarters_25 * 25);
-
- dimes _10 = cents / 10;
- cents = cents - (dimes_10 * 10);
-
- nickels_5 = cents / 5;
- cents = cents - (nickels_5 * 5);
-
- coin_2 = cents / 2;
- cents = cents - (coin_2 * 2);
-
- pennies_1 = cents / 1;
-
- change = quaerters_25 + dimes_10 + nickels_5 + coins_2 + pennies_1;
- printf("%d\n", change);
- return (0);
-}
