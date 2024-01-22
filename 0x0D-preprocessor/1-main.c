fe418efd1524925ef6bde0453c7acf55db581f25
KSelete <keketsoselete@gmail.com>
2024-01-22 12:29:13 -0800

    1-main.c

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,19 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - will check the code
+ *
+ * Retun: always = 0
+ */
+int main(void)
+{
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
+	return (0);
+}
