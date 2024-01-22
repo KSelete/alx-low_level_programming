2d856a7444ec7b9fae0d1eaea30eb24b7db64c95
KSelete <keketsoselete@gmail.com>
2024-01-22 12:37:19 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * Description: A program that prints the name of the file
+ * Return: Always = 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__)
+		return (0);
+}
