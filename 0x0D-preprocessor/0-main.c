dc0b5ab433afb80b7d392dd73caf53e1ade1e6b5
KSelete <keketsoselete@gmail.com>
2024-01-22 12:20:56 -0800

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - to check the code
+ *
+ * Return: always = 0
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
