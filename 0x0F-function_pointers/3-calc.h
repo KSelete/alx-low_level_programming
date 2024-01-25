d6fbb20c2ffcf86705527edab9923c9642f0baff
KSelete <keketsoselete@gmail.com>
2024-01-24 16:46:23 -0800

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,22 +1,22 @@
 #ifndef _STRUC_OPERATOR_
 #define _STRUC_OPERATOR_
-/**
- * struct_op - a struct op
- *
- * @op: this is the operator
- * @f: an associated function
- */
-typedef struct op
-{
-	char *op;
-	int (*f)(int a, int b);
-}
-op_t;
+
+int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 
+/**
+ * struct op -struct op
+ *
+ * @op- The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 #endif
