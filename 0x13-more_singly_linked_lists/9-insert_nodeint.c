c387a49ead227175cba79f34402ba7b72f251fc6
KSelete <keketsoselete@gmail.com>
2024-02-06 08:43:41 -0800

    insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t linked list.
+ * insert_nodeint_at_index - Inserts a new node at a given position.
  * @head: Pointer to the pointer to the head of the list.
  * @idx: Index of the list where the new node should be added.
  * @n: Value to store in the new node.
@@ -10,31 +10,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    unsigned int i;
-    listint_t *new_node, *current;
+	unsigned int i;
+	listint_t *new_node, *current;
 
-    if (head == NULL)
-        return NULL;
+	if (head == NULL)
+		return (NULL);
 
-    if (idx == 0)
-        return add_nodeint(head, n);
+	if (idx == 0)
+		return (add_nodeint(head, n));
 
-    current = *head;
+	current = *head;
 
-    for (i = 0; i < idx - 1 && current != NULL; i++)
-        current = current->next;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
 
-    if (current == NULL)
-        return NULL;
+	if (current == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return NULL;
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = current->next;
-    current->next = new_node;
+	new_node->n = n;
+	new_node->next = current->next;
+	current->next = new_node;
 
-    return new_node;
+	return (new_node);
 }
-
