f2d7555e56c40dab0b52e4cd013f51e4954ec5d9
KSelete <keketsoselete@gmail.com>
2024-02-08 09:33:32 -0800

    2-main.c

---
+++ 0x14-bit_manipulation/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+    int n;
+
+    n = get_bit(1024, 10);
+    printf("%d\n", n);
+    n = get_bit(98, 1);
+    printf("%d\n", n);
+    n = get_bit(1024, 0);
+    printf("%d\n", n);
+    return (0);
+}
+
