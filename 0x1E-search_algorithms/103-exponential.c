68fdf30fb8b46b89af035396dd912fb9522cd00e
KSelete <keketsoselete@gmail.com>
2024-07-17 14:50:35 -0700

    search_algorithms

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,103 +1,82 @@
 #include "search_algos.h"
 
 /**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
- *
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * print_array - to print an array
+ * @array: the array to print
+ * @l: the left index
+ * @r: the right index
  */
-int recursive_search(int *array, size_t size, int value)
+void print_array(int *array, int l, int r)
 {
-	size_t half = size / 2;
-	size_t i;
+	int i;
 
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array");
-
-	for (i = 0; i < size; i++)
-		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-
+	for (i = l; i <= r; i++)
+	{
+		printf("%d", array[i]);
+		if (i < r)
+			printf(", ");
+	}
 	printf("\n");
-
-	if (half && size % 2 == 0)
-		half--;
-
-	if (value == array[half])
-		return ((int)half);
-
-	if (value < array[half])
-		return (recursive_search(array, half, value));
-
-	half++;
-
-	return (recursive_search(array + half, size - half, value) + half);
 }
 
 /**
- * binary_search - calls to binary_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int binary_search(int *array, size_t size, int value)
-{
-	int index;
-
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
-}
-
-/**
- * exponential_search - searches for a value in an array of
- * integers using the Exponential search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
+* exponential_search -	the function that searches for a value in
+* an array of integers using the exponential search algorithm
+* @array: array of integers
+* @size: size of array
+* @value: value that i searched in array
+* Return: return the index of value, and if it doesn't exist return -1
+*/
 int exponential_search(int *array, size_t size, int value)
 {
-	size_t index, next;
-	int result;
+	int index, prev;
 
-	if (array == NULL)
+	if (!array || size == 0)
 		return (-1);
 
 	if (array[0] == value)
 		return (0);
-
-	index = 1;
-
-	while (array[index] < value && index < size)
+	for (index = 1; index <= (int) size; index = index * 2)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)index, array[index]);
-		index *= 2;
+		if (value <= array[index])
+			break;
+		prev = index;
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 	}
+	if (index >= (int) size)
+		index = (int) size - 1;
+	printf("Value found between indexes [%d] and [%d]\n", prev, index);
+	index = binary_search(array + prev, index - prev + 1, value);
+	if (index < (int) size && index >= 0)
+		return (index + prev);
+	else
+		return (-1);
+}
 
-	next = (index >= size) ? (size - 1) : index;
+/**
+* binary_search - the function that searches for a value in
+* an array of integers using the binary search algorithm
+* @array: array of integers
+* @size: size of array
+* @value: value that i searched in array
+* Return: return the index of value, and if it doesn't exist return -1
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0, right = (int) size - 1;
 
-	index /= 2;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
 
-	printf("Value found between indexes [%d] and [%d]\n", (int)index, (int)next);
-
-	result = binary_search(array + index, (next + 1) - index, value);
-
-	if (result >= 0)
-		result += index;
-
-	return (result);
+		printf("Searching in array: ");
+		print_array(array, left, right);
+		if (array[mid] == value)
+			return (mid);
+		if (value <= array[mid])
+			right = mid - 1;
+		else if (value > array[mid])
+			left = mid + 1;
+	}
+	return (-1);
 }
